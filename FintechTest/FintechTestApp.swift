686d38df13f27ad26ee9a2f9ac4301a3579ed757
PTM83 <torres.m.pablo@gmail.com>
2024-07-29 23:40:20 -0400

    Initial Commit

---
+++ FintechTest/FintechTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FintechTestApp.swift
+//  FintechTest
+//
+//  Created by Pablo Andrés Torres Montes on 29-07-24.
+//
+
+import SwiftUI
+
+@main
+struct FintechTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
