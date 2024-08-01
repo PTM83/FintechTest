38c195d2487a3a2ceb1c738705e9b3be7caa679b
PTM83 <torres.m.pablo@gmail.com>
2024-08-01 00:12:58 -0400

    Se agregan en comentario la paleta de colores a utilizar mediante colores RGB

--- FintechTest/DataConstantApp.swift
+++ FintechTest/DataConstantApp.swift
@@ -5,6 +5,15 @@
 //  Created by Pablo Andrés Torres Montes on 30-07-24.
 //
 
+/*
+ Paleta de Colores
+ Fondo Principal = RGB(213, 160, 33)
+ Fondo Blanco = RGB(237, 231, 217)
+ Letra Principal = RGB(75, 66, 55)
+ Letra Secundaria = RGB(164, 150, 148)
+ Letra Terceria = RGB(115, 107, 96)
+*/
+
 import SwiftUI
 
 // Estructura para encapsular los colores
@@ -17,4 +26,5 @@ struct CustomColors {
 struct CustomElements {
     static let elements: Array<Any> = Array(1...9) + [Image(systemName: "delete.left"), 0]
 }
+
 
