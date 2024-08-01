72720778ed7aa22d5534b468cf787dba0ad607a2
PTM83 <torres.m.pablo@gmail.com>
2024-08-01 00:10:04 -0400

    Se generá la vista que permite checkear el password y permite que los círculos pasen de vacío a lleno.

---
+++ FintechTest/checkPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  checkPasswordView.swift
+//  FintechTest
+//
+//  Created by Pablo Andrés Torres Montes on 31-07-24.
+//
+
+import SwiftUI
+
+struct checkPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    checkPasswordView()
+}
