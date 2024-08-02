20310d9ea6d1ceaf300de0faed3aecd45620ac81
PTM83 <torres.m.pablo@gmail.com>
2024-08-02 16:55:40 -0400

    La vista checkPasswordView se modifica y se crea la vista DigitEntryView que presenta la lógica de si el password ingresado es el correcto o no.

---
+++ FintechTest/Components/CheckPasswordView/DigitEntryView.swift
@@ -0,0 +1,18 @@
+//
+//  DigitEntryView.swift
+//  FintechTest
+//
+//  Created by Pablo Andrés Torres Montes on 02-08-24.
+//
+
+import SwiftUI
+
+struct DigitEntryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DigitEntryView()
+}
