32dffcfaccb88a419d01dad2d18a94930347ad93
PTM83 <torres.m.pablo@gmail.com>
2024-08-04 00:37:33 -0400

    Se desglosa el código en diferentes funciones para una mejor visibilidad.

--- FintechTest/Components/CheckPasswordView/DigitEntryView.swift
+++ FintechTest/Components/CheckPasswordView/DigitEntryView.swift
@@ -16,38 +16,54 @@ struct DigitEntryView: View {
     var body: some View {
         print("DigitEntryView body is being rendered")
         return VStack {
-            HStack {
-                ForEach(0..<4, id: \.self) { index in
-                    index < enteredDigits.count ? Image(systemName: "circle.fill") : Image(systemName: "circle")
-                }
-            }
+            createDigitIndicator()
             .padding(.bottom, 10)
             .onChange(of: enteredDigits) {
-                if enteredDigits.count == 4 {
-                    
-                    if attemptManager.attempCount < attemptManager.maxAttemps {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                            verifyPassword()
-                            }
-                    }
-                } else if showError && enteredDigits.count > 0 {
-                    showError = false // Resetea el error y elimina el mensaje
-                }
+                handleDigitChange()
             }
             if showError {
-                Text(attemptManager.attempCount >= attemptManager.maxAttemps ? "Demasiados intentos fallidos. Bloqueado." : "Has ingresado un PIN incorrecto. \nTe quedan \(attemptManager.diffAttemps) intentos")
-                    .foregroundColor(.white)
-                    .font(.system(size: 18,
-                                  weight: .ultraLight,
-                                  design: .serif))
-                    .frame(width: 300, height: 60, alignment: .center)
-                    .background(Color.red)
-                    .cornerRadius(10)
-                    .multilineTextAlignment(.center)
+                createErrorMessage()
             }
         }.frame(width: 300, height: 120, alignment: .top)
     }
     
+    // Función que permite crear el indicador de ingreso de código
+    private func createDigitIndicator() -> some View {
+        HStack {
+            ForEach(0..<4, id: \.self) { index in
+                index < enteredDigits.count ? Image(systemName: "circle.fill") : Image(systemName: "circle")
+            }
+        }
+    }
+    
+    // Función que verifica sí al ingresar los 4 dígitos el código es correcto o no.
+    private func handleDigitChange() {
+        
+        if enteredDigits.count == 4 {
+            if attemptManager.attempCount < attemptManager.maxAttemps {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    verifyPassword()
+                    }
+            }
+        } else if showError && enteredDigits.count > 0 {
+            showError = false // Resetea el error y elimina el mensaje
+        }
+    }
+    
+    // Función que renderiza el mensaje de Error
+    private func createErrorMessage() -> some View {
+        Text(attemptManager.attempCount >= attemptManager.maxAttemps ? "Demasiados intentos fallidos. Bloqueado." : "Has ingresado un PIN incorrecto. \nTe quedan \(attemptManager.diffAttemps) intentos")
+            .foregroundColor(.white)
+            .font(.system(size: 18,
+                          weight: .ultraLight,
+                          design: .serif))
+            .frame(width: 300, height: 60, alignment: .center)
+            .background(Color.red)
+            .cornerRadius(10)
+            .multilineTextAlignment(.center)
+    }
+    
+    // Función que permite verficiar si el código es correcto o no.
     private func verifyPassword() {
         if userDataBase.VerifyCredentials(password: enteredDigits) {
             print("Authentication successful")
