4eda6096ba7a9ca46c62f4960ec35795599d83a6
PTM83 <torres.m.pablo@gmail.com>
2024-08-04 02:40:17 -0400

    En la vista ViewIntroduction se agrega la Navegación con NavigationStack, el cual se debe pasar el .navigationDestination que toma un Bool, donde si es verdad se cambia de vista.

---
+++ FintechTest/Views/MachMainView.swift
@@ -0,0 +1,18 @@
+//
+//  MachMainView.swift
+//  FintechTest
+//
+//  Created by Pablo Andrés Torres Montes on 04-08-24.
+//
+
+import SwiftUI
+
+struct MachMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MachMainView()
+}
