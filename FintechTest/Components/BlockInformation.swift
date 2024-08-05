371e303ddfbfeba34524b0e7e4fdeae28d447c78
PTM83 <torres.m.pablo@gmail.com>
2024-08-05 17:24:45 -0400

    Se termina el cuadro de dialogo inicial. Pero falta agregar la animación para así ocularte el boton de Ver mis movimientos

--- FintechTest/Components/BlockInformation.swift
+++ FintechTest/Components/BlockInformation.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct BlockInformation: View {
     var frameWidth: CGFloat
     var frameHeight: CGFloat
+    @State private var colorPrincipal: Color = CustomColors.primaryColor.toSwiftColor()
+    
     
     var body: some View {
         RoundedRectangle(cornerRadius: 10)
@@ -17,7 +19,13 @@ struct BlockInformation: View {
             .frame(width: frameWidth, height: frameHeight)
             .shadow(color: CustomColors.digitColor.toSwiftColor().opacity(0.5), radius: 10, y: 5)
             .overlay(
-                BoldTextBlock(SecondText: "Saldo disponible", valueText: "$ 15.953")
+                
+                completeTextBlock(
+                    PrincipalText: "Mi cuenta MACH",
+                    underlineText: "Cuenta Corriente BCI 7771 12 345 678",
+                    SecondText: "Saldo disponible",
+                    valueText: "$ 15.953",
+                    colorPrincipal: colorPrincipal)
             )
     }
 }
@@ -26,15 +34,19 @@ struct BlockInformation: View {
     BlockInformation(frameWidth: 360, frameHeight: 200)
 }
 
-struct BoldTextBlock: View {
+struct completeTextBlock: View {
     
+    var PrincipalText: String
+    var underlineText: String
     var SecondText: String
     var valueText: String
+    var colorPrincipal: Color
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
+            
             HStack(alignment:.firstTextBaseline) {
-                HeadBlockName(PrincipalText: "Mi cuenta MACH", underlineText: "Cuenta Corriente BCI 7771 12345678")
+                HeadBlockName(PrincipalText: PrincipalText, underlineText: underlineText)
                 
                 Spacer()
                 
@@ -49,14 +61,38 @@ struct BoldTextBlock: View {
             .font(.system(size: 18, weight: .light, design: .monospaced))
             .tracking(-0.9)
             
+            agregateText(secondText: SecondText,
+                         valueText: valueText,
+                         colorPrincipal: colorPrincipal)
+            
+            Rectangle()
+                .frame(width: .infinity, height: 1).foregroundColor(.gray)
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/,
+                   label: {
+                Text("Ver mis movimientos")
+                    .font(.system(size: 20, weight: .semibold, design: .rounded)).frame(maxWidth: .infinity, alignment: .center)
+                    .foregroundColor(colorPrincipal)
+            })
+            
+        }
+        .padding(20)
+    }
+    
+
+    struct agregateText: View {
+        
+        var secondText: String
+        var valueText: String
+        var colorPrincipal: Color
+        
+        var body: some View {
             VStack(alignment: .leading, spacing: 2) {
-                Text(SecondText)
+                Text(secondText)
                     .font(.system(size: 20, weight: .semibold, design: .rounded))
                 Text(valueText)
                     .font(.system(size: 38, weight: .bold, design: .rounded))
-            }.foregroundColor(CustomColors.primaryColor.toSwiftColor())
-            
+            }.foregroundColor(colorPrincipal)
         }
-        .padding()
     }
+    
 }
