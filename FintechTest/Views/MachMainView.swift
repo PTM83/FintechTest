5fb7d0c0c2f963fff054f4035f648c33dc21d2d0
PTM83 <torres.m.pablo@gmail.com>
2024-08-05 14:17:20 -0400

    Se agrega el bloque Información se procede a generar la vista para agregar Texto.

--- FintechTest/Views/MachMainView.swift
+++ FintechTest/Views/MachMainView.swift
@@ -18,21 +18,35 @@ struct MachMainView: View {
                alignment: .top)
         .background(CustomColors.primaryColor.toSwiftColor())
         .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .foregroundColor(CustomColors.letterColor.toSwiftColor())
-                .frame(width: 300,
-                       height: 200)
-                .shadow(color: CustomColors.digitColor.toSwiftColor().opacity(0.5), radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/, y: 5)
-                .padding()
+            BlockInformation()
+                .overlay(
+                    VStack {
+                        HStack {
+                            Text("Mi cuenta MACH")
+                            Spacer()
+                            Button(action: {
+                                
+                            }, 
+                                   label: {
+                                Image(systemName: IconApp.sharedInformation).rotationEffect(.degrees(-30))
+                                    .foregroundColor(.black)
+                            })
+                        }.padding(20)
+                            .font(.system(size: 18, weight: .light, design: .monospaced))
+                            .tracking(-1.9)
+                        Spacer()
+                    }
+                )
                 .padding(.top, 190)
         )
         .navigationBarBackButtonHidden(true) // Esconder el botón de regresar
         .navigationBarHidden(true) // También puedes esconder toda la barra de navegación si es necesario
         
         Spacer()
-        
-        // Vista del Footer
-        FooterMachView()
+            .safeAreaInset(edge: .bottom, spacing: 0) {
+                // Vista del Footer
+                FooterMachView()
+        }
         
     }
 }
