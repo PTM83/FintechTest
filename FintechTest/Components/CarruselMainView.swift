cd4f059e548e3b8a6664d5bef851c4beb6a4be06
PTM83 <torres.m.pablo@gmail.com>
2024-08-06 00:02:23 -0400

    Vista que permite configurar el carrusel de las opciones que se pueden optar en la App.

--- FintechTest/Components/CarruselMainView.swift
+++ FintechTest/Components/CarruselMainView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct CarruselMainView: View {
+    
+    var textTitle: String
+    @State var principalColor: Color = CustomColors.primaryColor.toSwiftColor()
+    @State var letterColor: Color = CustomColors.digitColor.toSwiftColor()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading, spacing: 10) {
+            Text(textTitle)
+                .font(.system(size: 20, weight: .light, design: .rounded))
+                .foregroundColor(letterColor)
+                .kerning(1.4)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 16) {
+                    ForEach(IconApp.carruselMainView, id: \.0) {
+                        text, icon in Button(action: {},
+                                             label: {
+                            ItemView(icon: icon,
+                                     text: text,
+                                     mainColor: principalColor)
+                        })
+                            
+                    }
+                }
+            }
+        }
     }
+    
+    struct ItemView: View {
+        let icon: String
+        let text: String
+        var mainColor: Color
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Image(systemName: icon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 40, height: 40)
+                    .foregroundStyle(mainColor)
+                    .padding([.leading, .top], 5)
+                Text(text)
+                    .font(.system(size: 16, weight: .medium, design: .rounded))
+                    .kerning(1.2)
+                    .foregroundStyle(.black)
+                    .padding([.leading, .trailing, .bottom],8)
+                    .multilineTextAlignment(.leading)
+            }
+            .frame(width: 120, height: 120)
+            .background(Color.white)
+            .cornerRadius(10)
+            .shadow(color: Color.black.opacity(0.3), radius: 5, x: 0, y: 2)
+        }
+    }
+    
 }
 
 #Preview {
-    CarruselMainView()
+    CarruselMainView(textTitle: "Test View")
 }
