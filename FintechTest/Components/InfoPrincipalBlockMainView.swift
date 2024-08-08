7ffe985123b4dcad50d6fec16c17e50cfd571fc9
PTM83 <torres.m.pablo@gmail.com>
2024-08-08 00:04:20 -0400

    Se crea la Vista InfoPrincipalBlockMainView para así trabajar de mejor forma el texto que va en la caja principal.

--- FintechTest/Components/InfoPrincipalBlockMainView.swift
+++ FintechTest/Components/InfoPrincipalBlockMainView.swift
@@ -9,7 +9,62 @@ import SwiftUI
 
 struct InfoPrincipalBlockMainView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+        /*
+         Parte del código que va toda la información
+         a desplegar en la vista inicial
+         */
+            VStack(alignment: .leading, spacing: 12){
+            
+                HStack(alignment: .firstTextBaseline){
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Mi cuenta MACH")
+                            .font(.system(size: 15, weight: .semibold, design: .rounded))
+                        Text("Cuenta Corriente BCI 7771 12 345 678")
+                            .font(.system(size: 12, weight: .regular, design: .rounded))
+                            .tracking(0.4)
+                    }
+                    Spacer()
+                    
+                    Button(action: {
+                        // Debe ir a una Vista
+                    }, label: {
+                        Image(systemName: "person")
+                    })
+                }.foregroundColor(.black)
+                
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Saldo disponible")
+                        .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        .foregroundColor(CustomColors.primaryColor.toSwiftColor())
+                    Text("$ 15.953")
+                        .font(.system(size: 38, weight: .bold, design: .rounded))
+                        .foregroundColor(CustomColors.primaryColor.toSwiftColor())
+                }
+                /*
+                 Parte del código que se despliega o no si el usuario
+                 empuja hacia abajo la pantalla
+                 */
+                Divider()
+                    .background(Color.black)
+                
+                Button(action: {
+                    // Acción del botón
+                }) {
+                    Text("Ver mis movimientos")
+                        .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .foregroundColor(CustomColors.primaryColor.toSwiftColor())
+                }
+                
+            }
+            .padding(15)
+
+        }
+//        .background(.blue) // VStack Global
+
     }
 }
 
