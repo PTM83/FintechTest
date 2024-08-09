6d34fcdcc48899586cb49fcf2868f9a9310f9cec
PTM83 <torres.m.pablo@gmail.com>
2024-08-09 00:18:52 -0400

    Vista que es un component de la Opción de Transferencia

--- FintechTest/Components/TransferComponent/WayofTransferView.swift
+++ FintechTest/Components/TransferComponent/WayofTransferView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct WayofTransferView: View {
+    
+    let mainColor:Color = CustomColors.primaryColor.toSwiftColor()
+    let digitColor: Color = CustomColors.digitColor.toSwiftColor()
+    var iconName:String = "laptopcomputer"
+    var description: String = "Test"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        BlockPutInformationView(percentWidth: 0.7, percentHeight: 0.89, xPosition: 0, yPosition: 0, shadowColor: digitColor)
+            .overlay(
+                
+                VStack(alignment: .leading, spacing:10){
+                    Image(systemName: iconName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 27, height: 27)
+                        .foregroundStyle(mainColor)
+                        .padding(.top,4)
+                    Text(description)
+                        .font(.system(size: 18, weight: .regular, design: .rounded))
+                        .kerning(0.2)
+                        .frame(maxWidth: 100, maxHeight: 50,alignment: .topLeading)
+                }.padding(1)
+                .frame(width: 100, height: 90, alignment: .leading)
+            )
     }
 }
 
 #Preview {
-    WayofTransferView()
+    WayofTransferView(iconName: "laptopcomputer", description: "Test")
 }
