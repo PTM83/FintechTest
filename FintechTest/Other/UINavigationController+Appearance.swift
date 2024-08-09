9ffce06a4089b1a0e405f12e6460f6a3659f8b7a
PTM83 <torres.m.pablo@gmail.com>
2024-08-09 00:17:48 -0400

    Vista que es una extensión para setBackIndicatorImage

--- FintechTest/Other/UINavigationController+Appearance.swift
+++ FintechTest/Other/UINavigationController+Appearance.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
-struct UINavigationController_Appearance: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    UINavigationController_Appearance()
-}
+//extension UINavigationController {
+//    open override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+////        let backArrowColor:Color = CustomColors.letterColor.toSwiftColor()
+//        let leftArrowColor:Color = CustomColors.letterColor.toSwiftColor()
+////        let mainColor:Color = CustomColors.primaryColor.toSwiftColor()
+//        
+//        let appearance = UINavigationBarAppearance()
+//        appearance.configureWithOpaqueBackground()
+//        
+//        // Personaliza la apariencia del botón de retroceso
+//        appearance.backButtonAppearance.normal.titleTextAttributes = [
+//            .foregroundColor: UIColor(leftArrowColor), // Color del texto
+//            .font: UIFont.systemFont(ofSize: 18, weight: .bold, width: .standard) // Fuente del texto
+//        ]
+//        
+//        // Personaliza la flecha de retroceso
+//        appearance.setBackIndicatorImage(UIImage(systemName: "arrow.left")?.withTintColor(.init(leftArrowColor),
+//                                                                                          renderingMode: .alwaysOriginal),
+//                                         transitionMaskImage: UIImage(systemName: "arrow.left"))
+//        
+//        // Aplica la apariencia a la barra de navegación
+//        navigationBar.standardAppearance = appearance
+//        navigationBar.compactAppearance = appearance
+//        navigationBar.scrollEdgeAppearance = appearance
+//    }
+//}
