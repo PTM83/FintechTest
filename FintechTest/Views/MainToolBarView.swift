d64421fd7f74e3813be45092be07cafbcc36005d
PTM83 <torres.m.pablo@gmail.com>
2024-08-09 11:40:44 -0400

    Se generá la vista para ser agregada en la vista principal de la App.

---
+++ FintechTest/Views/MainToolBarView.swift
@@ -0,0 +1,105 @@
+//
+//  MainToolBarView.swift
+//  FintechTest
+//
+//  Created by Pablo Andrés Torres Montes on 09-08-24.
+//
+
+import SwiftUI
+
+struct MainToolBarView<Content: View, Destination: View>: View {
+    
+    /// Vista que permite ingresar la siguiente información:
+    ///
+    /// Parámetros
+    ///mainColor: Color de fondo.
+    ///secondaryColor: Color de las letras.
+    ///personName: Nombre de la persona dueña de la cuenta.
+    ///
+    ///destinationOne: Corresponde al icono de menú
+    ///destinationTwo: Corresponde al icono de pregunta
+    
+    // Colores a Utilizar
+    let mainColor: Color = CustomColors.primaryColor.toSwiftColor()
+    let secondaryColor: Color = CustomColors.letterColor.toSwiftColor()
+    
+    // orden de las variables si importa
+    var personName: String
+    var destinationOne: () -> Destination
+    var destinationTwo: () -> Destination
+    var content: () -> Content
+    
+    private let constantSize: [String:CGFloat] = [
+        "iconSize":20,
+        "textSize":25,
+        "frameSize":40
+    ]
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // Acá debe ir el contenido que se desea Renderizar
+                content()
+                
+            }
+            .toolbar {
+                ToolbarItemGroup(placement: .topBarLeading) {
+                    NavigationLink(destination: destinationOne()) {
+                        Image(systemName: "text.alignleft")
+                    }
+                    
+                    HStack(alignment:.center, spacing: 5) {
+                        if let frameSize = constantSize["frameSize"] {
+                            Circle()
+                                .stroke(secondaryColor,
+                                        lineWidth: 2)
+                                .frame(width: frameSize,
+                                       height: frameSize)
+                                .overlay(
+                                    Image(systemName: "person.fill").foregroundColor(secondaryColor)
+                                        .scaledToFit()
+                                )
+                        }
+                        if let textSize = constantSize["textSize"] {
+                            Text("Hola \(personName)")
+                                .font(.system(size: textSize))
+                        }
+                    }
+                    
+                }
+                
+                ToolbarItem (placement: .topBarTrailing) {
+                    HStack(spacing: 0) {
+                        NavigationLink(destination: destinationTwo()) {
+                            Image(systemName: "bell")
+                        }
+                        NavigationLink(destination: destinationTwo()) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                    }
+                }
+            }.foregroundColor(secondaryColor)
+                .font(.system(size: constantSize["iconSize"] ?? 20,
+                              weight: .medium,
+                              design: .rounded))
+                .toolbarBackground(mainColor, for: .automatic)
+                .toolbarBackground(.visible, for: .navigationBar)
+        }
+    }
+}
+
+#Preview {
+    MainToolBarView(
+        personName: "Person",
+        destinationOne: {
+            TestView() // Primera vista de destino
+        },
+        destinationTwo: {
+            TestView() // Segunda vista de destino
+        },
+        content: {
+            Text("Test View")
+                .foregroundStyle(.black)
+        }
+    )
+}
